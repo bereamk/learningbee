ccb343c888a0e2b94e927f12375f56bba30c3654
Berea <151574625+bereamk@users.noreply.github.com>
2024-02-02 02:01:26 +0100

    Added First Project
    Created my first python program that felt useful. Created due to
    a specific task/assignment from uni, but liked the idea enough to
    customize it a little bit more and set it asside/store it here.

---
+++ 3_projects/2024/python/Loan_Calculator.py
@@ -0,0 +1,100 @@
+#===========================================================================
+#  *                                 INFO
+#
+#   Python code
+#   Name: Robinson, Berea
+#   Course/Section: CMSC 105/6387
+#   Date: 2024-01-30
+
+#   Objective: 
+#       This program is a call back to the loan calculator I did during week 2
+#       But more concise and calculations within its own functions.
+#
+#===========================================================================
+
+import datetime
+import textwrap
+
+#--------------------------------- FUNCTIONS --------------------------------
+# Welcome Information function
+def welcome_message(title: str):
+    today = datetime.date.today()
+
+    message = dict(
+        title = f"{title.upper()}",
+        text = textwrap.fill(
+            "This program will calculate the cost of the interest accrued over time, as well as the total amount of the future loan value."
+            )
+    )
+
+    # Formating variables
+    width = 70
+    hr_sep = "="
+    sm_hr_sep = "-"
+
+    return(
+        "\nDate: \t{today}"
+        "\n{hr}\n"
+        "{title: ^70}\n"
+        "{hr}\n\n"
+        "{text}"
+        .format(
+            hr = hr_sep * width,
+            sm_hr = sm_hr_sep * width,
+            today = today,
+            **message
+        )
+    )
+
+# Function that calculates loan
+def loanCalculation(interestRate1,loanAmount1,loanTime1,years1):
+    # Calculate the amount of interest accrued
+    interestTotal = loanAmount1 * interestRate1 * loanTime1
+
+    months = years1 * 12
+
+    # Calculate the total amount of future value/amount accumulated
+    loanTotal = ((interestRate1 * loanTime1) + 1) * loanAmount1
+
+    interestperMonth = (interestTotal/months)
+    loanperMonth = (loanTotal/months)
+
+    monthlyTotal = interestperMonth + loanperMonth
+
+    return interestTotal, loanTotal, interestperMonth, loanperMonth, monthlyTotal
+
+
+#===========================================================================
+#                                 Main
+#===========================================================================
+def main():
+    print(welcome_message("Berea's Loan/Interest Calculation program v2.0"))
+    print(
+        f"\nPlease enter and provide the following information...\n"
+        f"-----------------------------------------------------------------------"
+    )  
+
+    # Prompt user for loan interest rate
+    interestRate = float(input("\n What is your loan's interest rate (in decimal) ? \t    "))
+
+    # Prompt user for loan amount and get user response
+    loanAmount = float(input("\n What is the total loan amount ? \t    ")) #numeric input value 1
+
+    # Prompt user for time of the investment (in years)
+    loanTime = float(input("\n How many years is the loan for ? \t    ")) #numeric input value 2  
+
+    interestTotal, loanTotal, interestperMonth, loanperMonth, monthlyTotal = loanCalculation(interestRate,loanAmount,loanTime,loanTime)
+
+
+    # Print the results
+    print(
+        f"\n--- Result ------------------------------------------------------------\n\n"
+        f" The  total  amount  of  interest accrued over", int(loanTime), "years is:       $", f"{interestTotal:.2f}\n"
+        f" The future value/amount accumulated  for  your  loan  is:    \t$", f"{loanTotal:.2f}\n\n"
+        f" The total amount of interest per month is:   \t$", f"{interestperMonth:.2f}\n"
+        f" The   total   principle   per   month  is:   \t$", f"{loanperMonth:.2f}\n"
+        f" The    final     total     per      month:   \t$", f"{monthlyTotal:.2f}"
+    )
+
+#------------------------------- EXECUTE --------------------------------
+main()
